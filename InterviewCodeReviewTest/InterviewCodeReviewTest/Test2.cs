706e63c642acebd830320cea980ddc45c8ae1190
ravi bhatt <18ravibhatt@gmail.com>
2021-10-03 16:14:10 +1300

    Test2 :  Isolating each transactions so issue with one transaction does not affect other one.

--- InterviewCodeReviewTest/InterviewCodeReviewTest/Test2.cs
+++ InterviewCodeReviewTest/InterviewCodeReviewTest/Test2.cs
@@ -3,62 +3,74 @@ using System.Data.SqlClient;
 
 namespace InterviewCodeReviewTest
 {
-	public class Test2
-	{
-		// Record customer purchase and update customer reward programme
-		public Result UpdateCustomerHistory(Purchase customerPurchase)
-		{
-			var connPruchase = new SqlConnection("data source=TestPurchaseServer;initial catalog=PurchaseDB;Trusted_Connection=True");
-			var connReward = new SqlConnection("data source=TestRewardServer;initial catalog=RewardDB;Trusted_Connection=True");
+    public class Test2
+    {
+        // Record customer purchase and update customer reward programme
+        public Result UpdateCustomerHistory(Purchase customerPurchase)
+        {
+            var connPruchase = new SqlConnection("data source=TestPurchaseServer;initial catalog=PurchaseDB;Trusted_Connection=True");
+            var connReward = new SqlConnection("data source=TestRewardServer;initial catalog=RewardDB;Trusted_Connection=True");
 
-			var cmdPurchase = new SqlCommand("INSERT INTO dbo.Purchase..."); // omitted the columns
-			var cmdReward = new SqlCommand("INSERT INTO dbo.Reward..."); // omitted the columns
+            var cmdPurchase = new SqlCommand("INSERT INTO dbo.Purchase..."); // omitted the columns
+            var cmdReward = new SqlCommand("INSERT INTO dbo.Reward..."); // omitted the columns
 
-			SqlTransaction tranPurchase = null;
-			SqlTransaction tranReward = null;
+            SqlTransaction tranPurchase = null;
+            SqlTransaction tranReward = null;
+            bool isTranPurchaseSuccessfull = true;
+            bool isTranRewardSuccessfull = true;
+            try
+            {
+                connPruchase.Open();
+                tranPurchase = connPruchase.BeginTransaction();
+                cmdPurchase.ExecuteNonQuery();
+                tranPurchase.Commit();
+            }
+            catch (Exception ex)
+            {
+                tranPurchase.Rollback();
+                isTranPurchaseSuccessfull = false;
+            }
+            try
+            {
+                connReward.Open();
+                tranReward = connReward.BeginTransaction();
+                cmdReward.ExecuteNonQuery();
+                tranReward.Commit();
 
-			try
-			{
-				connPruchase.Open();
-				tranPurchase = connPruchase.BeginTransaction();
-				cmdPurchase.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                tranReward.Rollback();
+                isTranRewardSuccessfull = false;
+            }
+            if ((isTranPurchaseSuccessfull == isTranRewardSuccessfull) && isTranPurchaseSuccessfull == true)
+            {
+                return Result.Success();
+            }
+            else
+            {
+                return Result.Failed();
+            }
+        }
+    }
 
-				connReward.Open();
-				tranReward = connReward.BeginTransaction();
-				cmdReward.ExecuteNonQuery();
+    public class Purchase
+    {
+        // Some members
+    }
 
-				tranPurchase.Commit();
-				tranReward.Commit();
+    public class Result
+    {
+        public bool IsSuccessful { get; private set; }
 
-				return Result.Success();
-			}
-			catch (Exception ex)
-			{
-				tranPurchase.Rollback();
-				tranReward.Rollback();
+        public static Result Success()
+        {
+            return new Result { IsSuccessful = true };
+        }
 
-				return Result.Failed();
-			}
-		}
-	}
-
-	public class Purchase
-	{
-		// Some members
-	}
-
-	public class Result
-	{
-		public bool IsSuccessful { get; private set; }
-
-		public static Result Success()
-		{
-			return new Result { IsSuccessful = true };
-		}
-
-		public static Result Failed()
-		{
-			return new Result { IsSuccessful = false };
-		}
-	}
+        public static Result Failed()
+        {
+            return new Result { IsSuccessful = false };
+        }
+    }
 }
